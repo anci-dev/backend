fee96b7e5723732ba04b6568ec3b0a4feae7522b
walkianm <walkianm@gmail.com>
2020-07-21 19:51:13 -0400

    Removed React front-end from Anci

--- auth.js
+++ auth.js
@@ -4,6 +4,31 @@ var session = require('express-session');
 var router = express.Router();
 const passport = require('passport');
 const COOKIE = process.env.DOMAIN;
+const request = require('request');
+
+router.get('/get_token', function(req, res){
+	console.log(req.query.code);
+
+	// var data = "client_id=" + process.env.GITHUB_CLIENT;
+    // data += "client_secret" + process.env.GITHUB_SECRET;
+    // data += "code" + req.query.code;
+
+	request.post("https://github.com/login/oauth/access_token", {
+        json: {
+			client_id: process.env.GITHUB_CLIENT,
+			client_secret: process.env.GITHUB_SECRET,
+			code: req.query.code,
+		}
+    }, (error, response, body) => {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log(body);
+			res.header("Access-Control-Allow-Origin", "*");
+			res.send(JSON.stringify(body));
+		}
+	});
+})
 
 router.get('/logoff', function(req, res) {
 	res.clearCookie(COOKIE)
