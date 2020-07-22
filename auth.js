9833453d934f98a3e4c32dda8966dafd7cbc4b1c
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-07-21 21:53:06 -0400

    Remove unused passport things (new github flow)

--- auth.js
+++ auth.js
@@ -1,9 +1,6 @@
 
 var express = require('express');
-var session = require('express-session');
 var router = express.Router();
-const passport = require('passport');
-const COOKIE = process.env.DOMAIN;
 const request = require('request');
 
 router.get('/get_token', function(req, res){
@@ -28,27 +25,6 @@ router.get('/get_token', function(req, res){
 			res.send(JSON.stringify(body));
 		}
 	});
-})
-
-router.get('/logoff', function(req, res) {
-	res.clearCookie(COOKIE)
-	res.redirect('/')
-})
-
-router.get('/github', passport.authenticate('github'))
-
-router.get(
-	'/github/login/return',
-	passport.authenticate('github', { successRedirect: '/auth/setcookie', failureRedirect: '/' })
-)
-
-router.get('/setcookie', function(req, res) {
-	let data = {
-		user: req.session.passport.user.profile._json,
-		token: req.session.passport.user.token
-	}
-	res.cookie(COOKIE, JSON.stringify(data))
-	res.redirect('/')
 })
 
 module.exports = router;
