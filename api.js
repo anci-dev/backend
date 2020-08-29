37bc20f5082300f3ba7053f813b4193673dbb1c6
walkianm <walkianm@gmail.com>
2020-08-29 19:38:21 -0400

    Something idk it's been awhile

--- api.js
+++ api.js
@@ -4,28 +4,32 @@ const request = require('request-promise');
 const github = require('./github');
 const db = require('./db');
 
-function addRepoInfo(repo, extraData) {
-    // using 'integrated' as test value to determine whether the repo is active
-    repo.integrated = false;
+router.get("/getBuilds", async function(req, res) {
+    var data = await db.getBuilds(req.query.repo);
+    res.send(data.records);
+});
 
-    var matching = extraData.find(element => element.ID == repo.id);
-    if (matching) {
-        delete matching.ID;
-        repo = {...repo, ...matching};
-        repo.integrated = true;
+router.get("/getAllRepos", async function(req, res) {
+    function combineRepoInfo(repo, extraData) {
+        // using 'integrated' as test value to determine whether the repo is active
+        repo.integrated = false;
+
+        var matching = extraData.find(element => element.id == repo.id);
+        if (matching) {
+            repo = {...repo, ...matching};
+            repo.integrated = true;
+        }
+        return repo;
     }
-    return repo;
-}
 
-// Accepts a single repo or list of repos, and returns associated db information
-router.get("/repo_status", async function(req, res) {
+
     // get user's repos from github
     var repos = await github.getUserRepos(req.query.access_token);
     // get extra repo info from our db
     var dbRepoData = await db.getRepositoryInfo(repos.map(item => item.id));
 
     // combine data
-    repos = repos.map(item => addRepoInfo(item, dbRepoData.records));
+    repos = repos.map(item => combineRepoInfo(item, dbRepoData.records));
     res.send(repos);
 });
 
--- db.js
+++ db.js
@@ -1,16 +1,37 @@
 const request = require('request-promise');
 
+// Gets a full page of builds from the selected repository
+// Can optionally select a page number
+function getBuilds(repository, page=0) {
+    return request({
+        uri: `${process.env.DB_API_DOMAIN}api/build/getBuilds`,
+        method: "GET",
+        qs: {
+            "repoID": repository,
+            "page": page
+        },
+        json: true
+    })
+    .catch(function(err) {
+        console.log("Unable to retrieve builds.");
+    });
+}
+
 function getRepositoryInfo(repositories) {
     return request({
-        uri:`${process.env.DB_API_DOMAIN}api/repository`,
-        qs:{"repositories": repositories},
-        json: true}
-    );
+        uri: `${process.env.DB_API_DOMAIN}api/repository`,
+        method: "GET",
+        qs: {"repositories": repositories},
+        json: true
+    })
+    .catch(function(err) {
+        console.log("Unable to retrieve repo info.");
+    });
 }
 
 function addUserToDB(userID) {
     request({
-        uri:`${process.env.DB_API_DOMAIN}api/profile/${userID}/createUser`,
+        uri: `${process.env.DB_API_DOMAIN}api/profile/${userID}/createUser`,
         method: "POST",
         json: true,
     })
@@ -20,4 +41,4 @@ function addUserToDB(userID) {
     });
 }
 
-module.exports = {addUserToDB, getRepositoryInfo};
+module.exports = {addUserToDB, getRepositoryInfo, getBuilds};
